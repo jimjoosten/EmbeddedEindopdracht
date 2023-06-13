36075d242d4ff4123fb6f14a6f193cb22e23e9b4
jimjoosten <jimjoosten1932@icloud.com>
2023-06-13 10:34:47 +0200

    Update buzzer.c

--- mainOpdracht/Core/Src/buzzer.c
+++ mainOpdracht/Core/Src/buzzer.c
@@ -6,7 +6,7 @@
 void Play_Buzzer()
 {
 	    HAL_GPIO_WritePin(BUZZER_GROVE_GPIO_Port, BUZZER_GROVE_Pin, GPIO_PIN_SET);
-	    HAL_Delay(25);
+	    HAL_Delay(500);
 	    HAL_GPIO_WritePin(BUZZER_GROVE_GPIO_Port, BUZZER_GROVE_Pin, GPIO_PIN_RESET);
-	    HAL_Delay(150);
+	    HAL_Delay(250);
 }
