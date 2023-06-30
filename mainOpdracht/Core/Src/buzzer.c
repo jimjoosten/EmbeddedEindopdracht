b6a83c111b3048fdc2060578aed70485bc584203
Jesperr <Info.brokeneu@gmail.com>
2023-06-30 09:18:25 +0200

    some bug fixing

--- mainOpdracht/Core/Src/buzzer.c
+++ mainOpdracht/Core/Src/buzzer.c
@@ -10,12 +10,13 @@ void Play_Buzzer(bool buzzer)
 {
 	if (buzzer){
 		iBuzzTeller++;
-		if (iBuzzTeller >= 30000 && bAlreadyPlayed == 0) {
+		if (iBuzzTeller >= 40000 && !bAlreadyPlayed) {
 			HAL_GPIO_WritePin(BUZZER_GROVE_GPIO_Port, BUZZER_GROVE_Pin, GPIO_PIN_SET);
 			bAlreadyPlayed = true;
-		} else if (iBuzzTeller >= 60000 && bAlreadyPlayed == 1) {
+		} else if (iBuzzTeller >= 80000 && bAlreadyPlayed) {
 			HAL_GPIO_WritePin(BUZZER_GROVE_GPIO_Port, BUZZER_GROVE_Pin, GPIO_PIN_RESET);
 			bAlreadyPlayed = false;
+			iBuzzTeller = 0;
 		}
 	}
 }
