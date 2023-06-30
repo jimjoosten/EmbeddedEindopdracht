9d6ea83f1e7ebcfd2bea12438510fa9aacf159f5
Jesperr <Info.brokeneu@gmail.com>
2023-06-30 09:14:52 +0200

    Alternative buzzer timeout code

--- mainOpdracht/Core/Src/buzzer.c
+++ mainOpdracht/Core/Src/buzzer.c
@@ -3,12 +3,19 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+int iBuzzTeller = 0;
+bool bAlreadyPlayed = false;
+
 void Play_Buzzer(bool buzzer)
 {
 	if (buzzer){
-	    HAL_GPIO_WritePin(BUZZER_GROVE_GPIO_Port, BUZZER_GROVE_Pin, GPIO_PIN_SET);
-	    HAL_Delay(500);
-	    HAL_GPIO_WritePin(BUZZER_GROVE_GPIO_Port, BUZZER_GROVE_Pin, GPIO_PIN_RESET);
-	    HAL_Delay(250);
+		iBuzzTeller++;
+		if (iBuzzTeller >= 30000 && bAlreadyPlayed == 0) {
+			HAL_GPIO_WritePin(BUZZER_GROVE_GPIO_Port, BUZZER_GROVE_Pin, GPIO_PIN_SET);
+			bAlreadyPlayed = true;
+		} else if (iBuzzTeller >= 60000 && bAlreadyPlayed == 1) {
+			HAL_GPIO_WritePin(BUZZER_GROVE_GPIO_Port, BUZZER_GROVE_Pin, GPIO_PIN_RESET);
+			bAlreadyPlayed = false;
+		}
 	}
 }
